04e74a2550fcdfcdba20c8cb9d1e962104027230
Jérémy Dufraisse <jeremy.dufraisse-info@orange.fr>
2023-11-27 11:41:09 +0100

    fix(accordion): first step to make it work

--- javascripts/bs5.js
+++ javascripts/bs5.js
@@ -18,35 +18,51 @@ document.addEventListener('DOMContentLoaded', function() {
   $(".dropdown-menu > li > a").addClass("dropdown-item")
 
   const convertingAddingClass= {
-    '.collapse.in': 'show'
+    '.collapse.in': 'show',
+    // '.panel-group': 'accordion',
+    // '.panel-group > .panel': 'accordion-item',
+    // '.panel-group > .panel > .panel-heading': ['accordion-header','accordion-button'],
+    // '.panel-group > .panel > .panel-collapse': 'accordion-collapse',
+    // '.panel-group > .panel > .panel-collapse > .panel-body': 'accordion-body',
   }
   Object.entries(convertingAddingClass)
     .forEach(([origin,newValue])=>{
       document.querySelectorAll(origin).forEach((e)=>{
-        e.classList.add(newValue)
+        const vals = Array.isArray(newValue) ? newValue : [newValue]
+        vals.forEach((v)=>{
+          e.classList.add(v)
+        })
       })
     })
   const convertingAttrs = {
     'data-toggle': {
       criteron: 'collapse',
-      newname: 'data-bs-toggle'
+      newname: 'data-bs-toggle',
+      replace: true
     },
     'data-parent': {
-      newname: 'data-bs-parent'
+      newname: 'data-bs-parent',
+      replace: true
     },
     'data-target': {
-      newname: 'data-bs-target'
+      newname: 'data-bs-target',
+      replace: true
     }
   }
   Object.entries(convertingAttrs)
     .forEach(([origin,data])=>{
         document.querySelectorAll(`[${origin}${data?.criteron?.length > 0 ? `="${data.criteron}"` : ''}]`).forEach((e)=>{
-          e.setAttribute(
-            data.newname,
-            data?.newcriteron
-              ?? data?.criteron
-              ?? e.getAttribute(origin)
-          )
+          if (data?.remove !== true){
+            e.setAttribute(
+              data.newname,
+              data?.newcriteron
+                ?? data?.criteron
+                ?? e.getAttribute(origin)
+            )
+          }
+          if (data?.remove === true || data?.replace === true){
+            e.removeAttribute(origin)
+          }
       })
     })
 })
