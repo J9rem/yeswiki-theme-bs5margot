8d4bfa417daa26a259a87bc1b00f97c80d0622d5
Jérémy Dufraisse <jeremy.dufraisse-info@orange.fr>
2024-01-02 17:51:02 +0100

    refactor(bs5.js): does not use jquery and run twice

--- javascripts/bs5.js
+++ javascripts/bs5.js
@@ -6,56 +6,29 @@
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
-document.addEventListener('DOMContentLoaded', function() {
 
-  // configure topnav with new classes
-  $("#yw-topnav .navbar-collapse").attr("id","navbarSupportedContent")
-  $("#navbarSupportedContent .navbar-nav > li").addClass("nav-item")
-  $("#navbarSupportedContent .navbar-nav > li > a").addClass("nav-link")
+/**
+ * create and execute a function to mak the code private
+ */
+(function(){
+  /**
+   * create a function modifiying the DOM to maintain compatibility oh HTML Bootstrap3 with 
+   * html bootstrap 5
+   */
 
-  // only one level
-  $('#yw-topnav div > ul.nav.navbar-nav > li > ul.dropdown-menu').each(function(){
-    const firstSiblingLink = $(this).siblings('a').first()
-    const href = firstSiblingLink?.attr('href') ?? ''
-    if (firstSiblingLink?.length > 0){
-      if (!(href?.length > 0) || href === '#'){
-        const firstLink = $(this).find('li > a[href^="http"]').first()
-        const firstHref = firstLink?.attr('href') ?? ''
-        if (firstHref?.length > 0){
-          firstSiblingLink.attr('href',firstHref)
-        }
-      }
-      firstSiblingLink.removeAttr('data-bs-toggle')
-      firstSiblingLink.removeAttr('data-toggle')
-      firstSiblingLink.removeClass('dropdown-toggle')
-      firstSiblingLink.parent().removeClass('dropdown')
-    }
-  })
-
-  // manage dropdown 
-  $(".dropdown-toggle").attr("data-bs-toggle","dropdown").attr("role","button").attr("aria-expanded","false")
-  $(".dropdown-menu > li > a").addClass("dropdown-item")
-
-  // sidebar button
-  $(".theme-sidebar > a,.theme-sidebar > .include > a").each(function(){
-    const txt = $(this).text()
-    const formattedTxt = txt.length > 0 ? `<span>${txt}</span>` : ''
-    const icon = $(this).find('i').first()
-    const formattedIcon = icon.length === 0 ? `<i class="fas fa-link"></i>` : icon
-    $(this).html('')
-    $(this).append(formattedTxt)
-    $(this).append(formattedIcon)
-  })
-
-  const convertingAddingClass= {
-    '.collapse.in': 'show',
-    // '.panel-group': 'accordion',
-    // '.panel-group > .panel': 'accordion-item',
-    // '.panel-group > .panel > .panel-heading': ['accordion-header','accordion-button'],
-    // '.panel-group > .panel > .panel-collapse': 'accordion-collapse',
-    // '.panel-group > .panel > .panel-collapse > .panel-body': 'accordion-body',
+  /**
+   * set helpers
+   */
+  const getTopNav = () => {
+    return document.getElementById('yw-topnav')
   }
-  Object.entries(convertingAddingClass)
+
+  const getSiblings = (nodeElement,query) => {
+    return nodeElement?.parentNode?.querySelectorAll(query) ?? null
+  }
+
+  const addClassAlias = (correspondances) => {
+    Object.entries(correspondances)
     .forEach(([origin,newValue])=>{
       document.querySelectorAll(origin).forEach((e)=>{
         const vals = Array.isArray(newValue) ? newValue : [newValue]
@@ -64,27 +37,10 @@ document.addEventListener('DOMContentLoaded', function() {
         })
       })
     })
-  const convertingAttrs = {
-    'data-toggle': {
-      criteron: ['collapse','tab','modal'],
-      newname: 'data-bs-toggle',
-      replace: true
-    },
-    'data-dismiss': {
-      criteron: ['modal'],
-      newname: 'data-bs-dismiss',
-      replace: true
-    },
-    'data-parent': {
-      newname: 'data-bs-parent',
-      replace: true
-    },
-    'data-target': {
-      newname: 'data-bs-target',
-      replace: true
-    }
   }
-  Object.entries(convertingAttrs)
+
+  const convertAttr = (params)=>{
+    Object.entries(params)
     .forEach(([origin,data])=>{
         const query = 
           (
@@ -106,11 +62,168 @@ document.addEventListener('DOMContentLoaded', function() {
           }
       })
     })
+  }
 
-    // manage alias for closing modal
-    $(document).on('click','.close[data-dismiss=modal]',function(){
-      $(this).removeAttr('data-dismiss')
-      $(this).attr('data-bs-dismiss','modal')
-      $(this).trigger('click')
+  /**
+   * configure Top Nav
+   */
+  const configureTopNav = () => {
+    const topNav = getTopNav()
+    const navbarCollapse = topNav.querySelector('.navbar-collapse:not(#navbarSupportedContent)')
+    navbarCollapse?.setAttribute('id','navbarSupportedContent')
+
+    navbarCollapse?.querySelectorAll('.navbar-nav > li:not(.nav-item)')?.forEach((item) => {
+      item.classList.add('nav-item')
     })
-})
+
+    navbarCollapse?.querySelectorAll('.navbar-nav > li > a:not(.nav-link)')?.forEach((item) => {
+      item.classList.add('nav-link')
+    })
+  }
+
+  const setOnlyOneLevel = () => {
+    const topNav = getTopNav()
+    const topNavDropdownMenus = topNav.querySelectorAll('div > ul.nav.navbar-nav > li > ul.dropdown-menu')
+    topNavDropdownMenus?.forEach((item) => {
+        const siblingLinks = getSiblings(item,'a')
+        const firstSiblingLink = siblingLinks?.[0] ?? null
+        const href = firstSiblingLink?.getAttribute('href') ?? ''
+        if (firstSiblingLink && !firstSiblingLink?.classList?.contains('already-dropdown-updated')){
+          firstSiblingLink.classList.add('already-dropdown-updated')
+          if (!(href?.length > 0) || href === '#'){
+            const links = item?.querySelectorAll('li > a[href^="http"]') ?? null
+            const firstLink = links?.[0] ?? null
+            const firstHref = firstLink?.getAttribute('href') ?? ''
+            if (firstHref?.length > 0){
+              firstSiblingLink.setAttribute('href',firstHref)
+            }
+          }
+          if (firstSiblingLink?.hasAttribute('data-bs-toggle')){
+            firstSiblingLink.removeAttribute('data-toggle')
+          }
+          if (firstSiblingLink?.hasAttribute('data-bs-toggle')){
+            firstSiblingLink.removeAttribute('data-toggle')
+          }
+          if (firstSiblingLink?.classList?.contains('dropdown-toggle')){
+            firstSiblingLink.classList.remove('dropdown-toggle')
+          }
+          if (firstSiblingLink?.parentNode?.classList?.contains('dropdown')){
+            firstSiblingLink.parentNode.classList.remove('dropdown')
+          }
+        }
+      }
+    )
+  }
+
+  /**
+   * dropdown
+   */
+  const manageDropDown = () => {
+    document.querySelectorAll('.dropdown-toggle:not([data-bs-toggle=dropdown])')?.forEach((item) => {
+        item.setAttribute("data-bs-toggle","dropdown")
+        item.setAttribute("role","button")
+        item.setAttribute("aria-expanded","false")
+      }
+    )
+    document.querySelectorAll('.dropdown-menu > li > a:not(.dropdown-item)').forEach((item) => {
+        item?.classList?.add("dropdown-item")
+      }
+    )
+  }
+
+  /**
+   * manage sidebar buttons
+   */
+  const manageSidebarbutton = () => {
+    document.querySelectorAll('.theme-sidebar > a:not(.sidebar-updated),.theme-sidebar > .include > a:not(.sidebar-updated)')?.forEach((item) => {
+        item?.classList.add('sidebar-updated')
+        const txt = item?.innerText ?? ''
+        const icon = item?.querySelector('i') ?? null
+
+        // clean
+        while (item.hasChildNodes()) {
+          item.removeChild(item.firstChild);
+        }
+        if (txt.length > 0){
+          const spanChild = document.createElement('span')
+          spanChild.appendChild(
+            document.createTextNode(txt)
+          )
+          item.appendChild(spanChild)
+        }
+        const formattedIcon = icon ?? document.createElement('i')
+        if (!icon){
+          formattedIcon.classList.add('fas')
+          formattedIcon.classList.add('fa-link')
+        }
+        item.appendChild(formattedIcon)
+      }
+    )
+  }
+
+  /**
+   * manage modals closing alias
+   */
+  const manageModalsClosing = () => {
+    document.addEventListener('click',(event)=>{
+      const target = event.target
+      if (target?.classList.contains('close')
+        && target?.hasAttribute('data-dismiss')
+        && target?.getAttribute('data-dismiss') === 'modal'){
+        target.removeAttribute('data-dismiss')
+        target.setAttribute('data-bs-dismiss','modal')
+        target.dispatchEvent(document.createEvent('click'))
+      }
+    })
+  }
+
+  /**
+   * main process
+   */
+  const mainFunc = (firstCall = false) => {
+    if (firstCall === true){
+      manageModalsClosing()
+      manageSidebarbutton()
+    }
+    configureTopNav()
+    setOnlyOneLevel()
+    manageDropDown()
+
+    addClassAlias({
+      '.collapse.in': 'show',
+      // '.panel-group': 'accordion',
+      // '.panel-group > .panel': 'accordion-item',
+      // '.panel-group > .panel > .panel-heading': ['accordion-header','accordion-button'],
+      // '.panel-group > .panel > .panel-collapse': 'accordion-collapse',
+      // '.panel-group > .panel > .panel-collapse > .panel-body': 'accordion-body',
+    })
+
+    convertAttr({
+      'data-toggle': {
+        criteron: ['collapse','tab','modal'],
+        newname: 'data-bs-toggle',
+        replace: true
+      },
+      'data-dismiss': {
+        criteron: ['modal'],
+        newname: 'data-bs-dismiss',
+        replace: true
+      },
+      'data-parent': {
+        newname: 'data-bs-parent',
+        replace: true
+      },
+      'data-target': {
+        newname: 'data-bs-target',
+        replace: true
+      }
+    })
+  }
+
+  // run once after DOMContentLoaded (after bootstrap if missing things)
+  document.addEventListener('DOMContentLoaded', mainFunc)
+
+  // run once now (before bootsrap)
+  mainFunc(true)
+
+})()
